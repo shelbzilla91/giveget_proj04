c9cd785c95ae2f9c043e76fecc3694a2231f4d63
Shelby Baker <shelbybaker@Shelbys-MacBook-Air.local>
2019-03-01 13:07:17 -0500

    asdf

---
+++ client/build/precache-manifest.cf985cf4bfeb3722a6b425943162cd4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd4f15c3511b30432c57",
+    "url": "/static/css/main.08791857.chunk.css"
+  },
+  {
+    "revision": "cd4f15c3511b30432c57",
+    "url": "/static/js/main.cd4f15c3.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "23bc51c9ebada08b0edf",
+    "url": "/static/css/2.aac73e19.chunk.css"
+  },
+  {
+    "revision": "23bc51c9ebada08b0edf",
+    "url": "/static/js/2.23bc51c9.chunk.js"
+  },
+  {
+    "revision": "3684aa24bcdf3be4e6b3e44d271f231f",
+    "url": "/index.html"
+  }
+];
