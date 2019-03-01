70768ef16870a4ab4bf21b35fba4149735c09bc0
Shelby Baker <shelbybaker@Shelbys-MacBook-Air.local>
2019-03-01 12:26:45 -0500

    editing file stuff fgfhg

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'projectmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'project04r',
         'USER': 'shelbybaker',
         'PASSWORD': 'CodeBridge',
